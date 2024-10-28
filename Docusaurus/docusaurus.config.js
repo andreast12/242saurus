9c68f1b8eec7cb5ed3aee3e80d8df7d38590e00a
andreast12 <andreast12.id@gmail.com>
2024-10-28 18:54:38 +0700

    modify docusaurus config

--- Docusaurus/docusaurus.config.js
+++ Docusaurus/docusaurus.config.js
@@ -20,8 +20,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'andreast12', // Usually your GitHub org/user name.
+  projectName: '242saurus', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
