6717d0e2387b249239f5a8a211011b0a3923fafc
andreast12 <andreast12.id@gmail.com>
2024-10-28 19:50:21 +0700

    modify docusaurus config

--- Docusaurus/docusaurus.config.js
+++ Docusaurus/docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://andreast12.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/242saurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'andreast12', // Usually your GitHub org/user name.
   projectName: '242saurus', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
